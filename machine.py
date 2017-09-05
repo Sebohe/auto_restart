41612ff250a0706cdf8d05668e8796ab9edddd84
Sebastian Bolanos <sebasheston@gmail.com>
2017-09-04 22:12:35 -0700

    Seperated funtiontions into what I think should be everything

--- machine.py
+++ machine.py
@@ -2,29 +2,90 @@
 
 import requests
 import os
+import smtplib
+from email.mime.text import MIMEText
+
+def obtainEnviromentVars():
+    
+    return {'URL':os.environ['EURL'],
+            'GMAIL_EMAIL': os.environ['GMAIL_EMAIL'],
+            'GMAIL_PASS':os.environ['GMAILS_PASS'],
+            'TO_EMAIL':os.environ['TO_EMAIL']}
 
 
-address = os.environ['ADDRESS']
-url = os.environ['EURL']
-name = os.environ['WORKER']
+def jsonStatus():
 
-requestX = requests.get(url)
+    requestX = requests.get(url)
 
-if requestX.status_code != 200:
-    print (requestX.status_code)
+    if requestX.status_code != 200:
+        #print (requestX.status_code)
+        raise
+
+    jsonX = requestX.json()
+    down = False
+
+    #print(jsonX)
+    
+    try:
+        if jsonX['data']['reportedHashrate'] == 0:
+            down = True
+    except:
+        down = True
+
+    return down
+
+def sendRestartEmails():
+    pass
+
+def sendRestartEmail():
+    pass
+
+def checkUpTime():
+   
+    with open('/proc/uptime','r') as f:
+        line = f.readline()
+   
+    print (line)
+    #the first column of the uptime file is the value we want
+    #https://www.centos.org/docs/5/html/5.1/Deployment_Guide/s2-proc-uptime.html
+    time = float(line.split(' ')[0])
+    print (time)
+    time = secondsToTime(time)
+    
+    print (time)
+
+    return time
+
+def secondsToTime(seconds):
+
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    
+    return "%d:%02d:%02d" % (h, m, s)
 
 
-jsonX = requestX.json()
+def restart():
 
-down = False
-
-print (jsonX)
-if jsonX['data']['reportedHashrate'] == 0:
-    down = True
-
-
-print (os.system('pgrep nm'))
-
-if down:
     print ('Machine is down, restart')
-    os.system('reboot')
+    os.system('pgrep ethminer | xargs kill')
+    os.system('systemctl reboot')
+
+
+if __name__ == "__main__":
+
+
+    checkUpTime()
+
+#    enviroment = obtainEnviromentVars()
+    
+#    status = jsonStatus(enviroment['EURL'])
+
+#    if status:
+#        os.sys.exit()
+#    else:
+#        sendRestartEmails()
+
+
+
+
+
