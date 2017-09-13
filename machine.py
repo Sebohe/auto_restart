5d6c2702e9bae4840b749a8cd27d516c732d74bc
Sebastian Bolanos <sebasheston@gmail.com>
2017-09-13 06:36:55 -0700

    Extended the e-mail commands

--- machine.py
+++ machine.py
@@ -5,15 +5,28 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 
+
+def checkClientStatus():
+    
+    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+
+    for pid in pids:
+        try:
+            print (open(os.path.join('/proc', pid, 'cmline'), 'rb').read())
+        except:
+            continue
+
+
+
 def obtainEnviromentVars():
     
-    return {'URL':os.environ['EURL'],
+    return {'EURL':os.environ['EURL'],
             'GMAIL_EMAIL': os.environ['GMAIL_EMAIL'],
-            'GMAIL_PASS':os.environ['GMAILS_PASS'],
+            'GMAIL_PASS':os.environ['GMAIL_PASS'],
             'TO_EMAIL':os.environ['TO_EMAIL']}
 
 
-def jsonStatus():
+def jsonStatusDown(url):
 
     requestX = requests.get(url)
 
@@ -34,27 +47,48 @@ def jsonStatus():
 
     return down
 
-def sendRestartEmails():
-    pass
+def sendRestartSuccesfulEmails(recipients, sender, password):
 
-def sendRestartEmail():
-    pass
+    body = 'Succesfuly restarted machine.'
+    subject = 'Restart succesful'
+    emailBuilder(recipients, sender, password, body, subject)
+
+
+def sendMachineDownEmails(recipients, sender, password):
+    
+    body = 'Machine is down, restarting machine.'
+    subject = 'Machine down'
+    emailBuilder(recipients, sender, password, body, subject)
+
+
+def emailBuilder(recipients, sender, password, body, subject):
+
+    #TODO, add raise exception and log it in the scenario that
+    #smtp fails
+    smtpServer = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpServer.ehlo()
+    smtpServer.starttls()
+    smtpServer.ehlo()
+    smtpServer.login(sender, password)
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipients
+    smtpServer.sendmail(sender, recipients, msg.as_string())
+    smtpServer.quit()
 
 def checkUpTime():
    
     with open('/proc/uptime','r') as f:
         line = f.readline()
    
-    print (line)
     #the first column of the uptime file is the value we want
     #https://www.centos.org/docs/5/html/5.1/Deployment_Guide/s2-proc-uptime.html
     time = float(line.split(' ')[0])
-    print (time)
     time = secondsToTime(time)
-    
-    print (time)
 
     return time
+
 
 def secondsToTime(seconds):
 
@@ -74,17 +108,26 @@ def restart():
 if __name__ == "__main__":
 
 
-    checkUpTime()
+    checkClientStatus()
 
-#    enviroment = obtainEnviromentVars()
+    hours, minutes, seconds =  checkUpTime().split(':')
+    if hours == '0' and minutes < '3':
+        #sendRestartSuccesfulEmails()
+        os.sys.exit()
+
+    envVars = obtainEnviromentVars()
     
-#    status = jsonStatus(enviroment['EURL'])
+    status = jsonStatusDown(envVars['EURL'])
 
-#    if status:
-#        os.sys.exit()
-#    else:
-#        sendRestartEmails()
+    if status:
+        sendMachineDownEmails(envVars['TO_EMAIL'], envVars['GMAIL_EMAIL'],
+                                envVars['GMAIL_PASS'])
 
+       #####restart()
+        pass
+    else:
+
+        os.sys.exit()
 
 
 
